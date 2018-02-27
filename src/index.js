80946548c38d12fe9f6324a6483cf9c9a03f8e25
Адамов Александр Михайлович <aadamov@cs.local>
2018-02-27 15:56:00 +0300

    hf

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const users = require('./users');
 
 const bot = new Bot({
   endpoints: ['wss://ws1.coopintl.com'],
-  username: 'testbot',
+  username: 'createtb',
   password: '666',
 });
 // const bot = new Bot({
